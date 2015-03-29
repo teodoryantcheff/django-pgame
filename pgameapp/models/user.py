ed02cf2d3cf9573cd82199810598e6ee32176fbe
Teodor Yantcheff <teodor.yantchev@gmail.com>
2015-03-30 01:17:06 +0300

    Added User a proxy class

---
+++ pgameapp/models/user.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+from custom_user.models import EmailUser
+
+__author__ = 'Jailbreaker'
+
+
+class AdminsManager(models.Manager):
+    def get_queryset(self):
+        return super(AdminsManager, self).get_queryset().filter(is_staff=True)
+
+
+class HUIUser(EmailUser):
+    class Meta:
+        proxy = True
+
+    objects = models.Manager()
+    admins = AdminsManager()
+
+    def test(self):
+        print '{} test on HUIUser'.format(self)
