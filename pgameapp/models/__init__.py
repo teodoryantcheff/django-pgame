186f44f0b1e01f1b0b5a1fe22cf1d430caf501ac
Teodor Yantcheff <teodor.yantchev@gmail.com>
2015-03-30 22:07:12 +0300

    formatting

--- pgameapp/models/__init__.py
+++ pgameapp/models/__init__.py
@@ -2,7 +2,7 @@
 
 from django.conf import settings
 
-AUTH_USER_MODEL = getattr(settings, "AUTH_USER_MODEL", "auth.User")
+AUTH_USER_MODEL = getattr(settings, 'AUTH_USER_MODEL', 'auth.User')
 
 DECIMAL_DECIMAL_PLACES = 5
 DECIMAL_MAX_DIGITS = DECIMAL_DECIMAL_PLACES + 11
@@ -11,6 +11,9 @@ from actors import UserActorOwnership, Actor
 from userprofile import UserProfile
 from gameconfiguration import GameConfiguration
 from manualgamestats import ManualGameStats
+
+from user import User
+
 from history import *
 from models import *
 
