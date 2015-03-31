cbb53c37814237c37174c839ae2a9e143335b312
Teodor Yantcheff <teodor.yantchev@gmail.com>
2015-03-31 17:18:46 +0300

    Ugly and forced cast of request.user to a proxy model

--- pgameapp/middleware.py
+++ pgameapp/middleware.py
@@ -1,1 +1,11 @@
 __author__ = 'Jailbreaker'
+
+# http://stackoverflow.com/questions/3006753/how-to-convert-request-user-into-a-proxy-auth-user-class
+
+from pgameapp.models import User
+
+
+class CustomTUserMiddleware(object):
+    def process_request(self, request):
+        if hasattr(request, 'user') and request.user.is_authenticated():
+            request.user.__class__ = User
