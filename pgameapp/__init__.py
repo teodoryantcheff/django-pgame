472c266b3592cec283e519c0061ceb8e6cc9c6a9
Teodor Yantcheff <teodor.yantchev@gmail.com>
2015-04-02 02:27:27 +0300

    database settings update

--- pgameapp/__init__.py
+++ pgameapp/__init__.py
@@ -29,8 +29,10 @@ def activate_foreign_keys(sender, connection, **kwargs):
     """Enable integrity constraint with sqlite."""
     if connection.vendor == 'sqlite':
         cursor = connection.cursor()
-        cursor.execute('PRAGMA foreign_keys = OFF;')
+        cursor.execute('PRAGMA foreign_keys = ON;')
         cursor.execute('PRAGMA journal_mode = MEMORY;')
+        cursor.execute('PRAGMA main.temp_store = MEMORY;')
+        cursor.execute('PRAGMA cache_size = 8192;')  # in 1k pages
         # cursor.execute('PRAGMA cache_size = n_of_pages;')
 
 connection_created.connect(activate_foreign_keys)
