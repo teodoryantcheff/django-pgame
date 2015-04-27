12955ba7a9c0e5aeec2d14d3cc873a6951cf5330
Teodor Yantcheff <teodor.yantchev@gmail.com>
2015-04-27 11:51:37 +0300

    settings updates

--- pgame/settings.py
+++ pgame/settings.py
@@ -43,7 +43,7 @@ DJANGO_APPS = (
     'django.contrib.staticfiles',
 )
 
-# non-django, 3rd party apps
+# django non-core, 3rd party apps
 THIRD_PARTY_APPS = (
     'custom_user',        # EmailUser
     'debug_toolbar',      # DjDT
@@ -63,6 +63,35 @@ PROJECT_APPS = (
 )
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + PROJECT_APPS
+
+# # A sample logging configuration. The only tangible logging
+# # performed by this configuration is to send an email to
+# # the site admins on every HTTP 500 error when DEBUG=False.
+# # See http://docs.djangoproject.com/en/dev/topics/logging for
+# # more details on how to customize your logging configuration.
+# LOGGING = {
+#     'version': 1,
+#     'disable_existing_loggers': False,
+#     'filters': {
+#         'require_debug_false': {
+#             '()': 'django.utils.log.RequireDebugFalse'
+#         }
+#     },
+#     'handlers': {
+#         'mail_admins': {
+#             'level': 'ERROR',
+#             'filters': ['require_debug_false'],
+#             'class': 'django.utils.log.AdminEmailHandler'
+#         }
+#     },
+#     'loggers': {
+#         'django.request': {
+#             'handlers': ['mail_admins'],
+#             'level': 'ERROR',
+#             'propagate': True,
+#         },
+#     }
+# }
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
